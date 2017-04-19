97ab9fc1ca59f0dc0db324711ff68494face1b9e
CodeHeeler <codeheeler@gmail.com>
2017-04-19 16:03:49 -0400

    nested details on click work

--- js/star_wars.js
+++ js/star_wars.js
@@ -37,11 +37,47 @@ $('#movies').on('click', function(e){
 })
 // end of the movies callbacks
 
+function getPageCharacterDetails(data){
+  let details = ""
+  let name = data['name'];
+  let height = data['height'];
+  let mass = data['mass'];
+  let hair_color = data['hair_color'];
+  let skin_color = data['skin_color'];
+  let eye_color = data['eye_color'];
+  let birth_year = data['birth_year'];
+  let gender = data['gender'];
+  details = details + "Name: " + name + "</br>";
+  details = details + "Height: " + height + " cm</br>";
+  details = details + "Mass: " + mass + " kg</br>";
+  details = details + "Hair Color: " + hair_color + "</br>";
+  details = details + "Skin Color: " + skin_color + "</br>";
+  details = details + "Eye Color: " + eye_color + "</br>";
+  details = details + "Birth Year: " + birth_year + " (measured in years Before or After the Battle of Yavin)</br>";
+  details = details + "Gender: " + gender + "</br>";
+  return details;
+}
+
+function displayCharacterDetails(data){
+  console.log(data)
+  $('#myData').html(getPageCharacterDetails(data));
+}
+
+function moreCharacterDetails(data){
+  $('#myData').empty();
+  destination = data
+  $.ajax({
+    url: destination,
+    success:displayCharacterDetails,
+  });
+}
+
 function getPageCharacters(data){
   let people = ""
   for (var i = 0; i < data['results'].length; i++){
     let name = data['results'][i]['name'];
-    people = people + name + "</br>";
+    let url = data['results'][i]['url'];
+    people = people + "<li onclick='moreCharacterDetails(\"" + url + "\")'>" + name + "</li></br>";
   }
   return people;
 }
@@ -67,11 +103,53 @@ $('#characters').on('click', function(e){
 })
 // end of the characters
 
+function getPageVehicleDetails(data){
+  let details = ""
+  let name = data['name'];
+  let model = data['model'];
+  let manufacturer = data['manufacturer'];
+  let cost_in_credits = data['cost_in_credits'];
+  let length = data['length'];
+  let max_atmosphering_speed = data['max_atmosphering_speed'];
+  let crew = data['crew'];
+  let passengers = data['passengers'];
+  let cargo_capacity = data['cargo_capacity'];
+  let consumables = data['consumables'];
+  let vehicle_class = data['vehicle_class'];
+  details = details + "Name: " + name + "</br>";
+  details = details + "Model: " + model + "</br>";
+  details = details + "Manufacturer: " + manufacturer + "</br>";
+  details = details + "Cost: " + cost_in_credits + " Galactic Credits</br>";
+  details = details + "Length: " + length + " m</br>";
+  details = details + "Max Atmosphering Speed: " + length + " m/s</br>";
+  details = details + "Crew: " + crew + "</br>";
+  details = details + "Passengers: " + passengers + "</br>";
+  details = details + "Cargo Capacity: " + cargo_capacity + " kg</br>";
+  details = details + "Consumables: " + consumables + "</br>";
+  details = details + "Vehicle Class: " + vehicle_class + "</br>";
+  return details;
+}
+
+function displayVehicleDetails(data){
+  console.log(data)
+  $('#myData').html(getPageVehicleDetails(data));
+}
+
+function moreVehicleDetails(data){
+  $('#myData').empty();
+  destination = data
+  $.ajax({
+    url: destination,
+    success:displayVehicleDetails,
+  });
+}
+
 function getPageVehicles(data){
   let transports = ""
   for (var i = 0; i < data['results'].length; i++){
     let name = data['results'][i]['name'];
-    transports = transports + name + "</br>";
+    let url = data['results'][i]['url'];
+    transports = transports + "<li onclick='moreVehicleDetails(\"" + url + "\")'>" + name + "</li></br>";
   }
   return transports;
 }
